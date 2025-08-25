e1704d2a07f48d4ce5299b503b2f3b6b818a7d7d
Luis Butron <butron0407@gmail.com>
2025-08-25 13:23:51 -0600

    First commit

--- src/fizzbuzz.js
+++ src/fizzbuzz.js
@@ -1,4 +1,18 @@
 export function fizzbuzz(){
-  //tu código va aqui
-  
+  let numero;
+  for (numero  = 1; numero  <= 100; numero ++) {
+    if (numero % 3 == 0 && numero % 5 == 0) {
+      console.log("FizzBuzz");
+    } 
+    else if (numero % 3 == 0) {
+      console.log("Fizz");
+    } 
+    else if (numero % 5 == 0) {
+      console.log("Buzz");
+    } 
+    else {
+      console.log(numero);
+    }
+  }
 }
+fizzbuzz();
